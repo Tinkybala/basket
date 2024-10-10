4204b391d4a083e9cacf13f92df51746d721a6c5
tinkybala <nightcorex001@gmail.com>
2024-10-11 00:12:07 +0800

    added index.html, main.html, navbar.html, events_list.html, event_card.html to base/templates

--- app/basket/base/views.py
+++ app/basket/base/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.template import loader
+from event.models import Event
 
 # Create your views here.
 def main(request):
-    return render(request, "base/templates/main.html")
+    #get all events (by order in db for now)
+    events = Event.objects.all()
+    context = {
+        'events': events
+    }
+    return render(request, "base/main.html", context)
+
