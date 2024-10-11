8727e33d6e82e2dd89b89bc25ae9368b664b0752
Bryan <brya0021@e.ntu.edu.sg>
2024-10-11 18:34:39 +0800

    Created new branch and user profile page

--- app/basket/base/urls.py
+++ app/basket/base/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.main, name="main")
+    path("", views.main, name="main"),
+    path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
 ]
--- app/basket/user/views.py
+++ app/basket/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import User
+
+def profile(request, user_id):
+    user = User.objects.get(id=user_id)
+    return render(request, "user/profile.html", {"user": user})
 
 # Create your views here.
