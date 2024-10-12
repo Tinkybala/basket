cd250197b3210e09ce348f66d68f628345422440
Bryan <brya0021@e.ntu.edu.sg>
2024-10-12 18:07:43 +0800

    Added event pages and renamed user profile urls to profile/id

--- app/basket/base/urls.py
+++ app/basket/base/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from user import views as user_views
 
 from . import views
 
 urlpatterns = [
     path("", views.main, name="main"),
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('profile/<int:user_id>/', user_views.profile, name='profile'),
+    path('event/', include('event.urls')),
 ]
